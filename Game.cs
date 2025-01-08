87b8e06f583bc278e4efe81030abaa0ed9df9be9
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2025-01-08 19:02:03 +0100

    'Maelstorm' completado.

--- Game.cs
+++ Game.cs
@@ -20,6 +20,13 @@ namespace _31_FountainOfObjects
             [(2, 2), (4, 3)],
             [(2, 3),(2, 7),(5, 4),(7, 2)]
         };
+        static (int Row, int Column)[][] maelstormsPositionByDifficulty =
+        {
+            [(3, 1)],
+            [(0, 1)],
+            [(4, 2),(3, 6)]
+        };
+
         static (int Row, int Column) playerPosition;
         static bool gameCompleted = false;
         static bool fountainActivated = false;
@@ -74,6 +81,7 @@ namespace _31_FountainOfObjects
                         DisplayFountainText("You hear the rushing waters from the Fountain of Objects. It has ben reactivated!");
                     }
                 }
+
                 foreach ((int, int) pit in pitsPositionByDifficulty[difficulty])
                 {
                     if (playerPosition == pit)
@@ -91,8 +99,45 @@ namespace _31_FountainOfObjects
                 if (playerDead)
                     break;
 
+                foreach ((int, int) maelstorm in maelstormsPositionByDifficulty[difficulty])
+                {
+                    if (maelstorm.Item2 - playerPosition.Column >= -1 && maelstorm.Item2 - playerPosition.Column <= 1
+                        && maelstorm.Item1 - playerPosition.Row >= -1 && maelstorm.Item1 - playerPosition.Row <= 1)
+                    {
+                        DisplayDescriptiveText("You hear the growling and groaning of a maelstorm nearby.");
+                    }
+                }
+
                 Console.Write("What do you want to do? ");
                 EvaluatePlayerActionsInGameLoop();
+
+                for (int i = 0; i < maelstormsPositionByDifficulty[difficulty].Length; i++)
+                {
+                    (int, int) maelstorm = maelstormsPositionByDifficulty[difficulty][i];
+                    if (playerPosition == maelstorm)
+                    {
+                        if (maelstorm.Item1 >= rooms[0].Length - 2 && maelstorm.Item2 <= 2)
+                            maelstorm = (rooms[0].Length - 1, 0);
+                        else if (maelstorm.Item1 >= rooms[0].Length - 2 && maelstorm.Item2 > 2)
+                            maelstorm = (rooms[0].Length - 1, maelstorm.Item2 - 2);
+                        else if (maelstorm.Item1 < rooms[0].Length - 2 && maelstorm.Item2 <= 2)
+                            maelstorm = (++maelstorm.Item1, 0);
+                        else
+                            maelstorm = (++maelstorm.Item1, maelstorm.Item2 - 2);
+
+                        if (playerPosition.Row <= 1 && playerPosition.Column >= rooms[1].Length - 2)
+                            playerPosition = (0, rooms[i].Length - 1);
+                        else if (playerPosition.Row <= 1 && playerPosition.Column <= rooms[1].Length - 2)
+                            playerPosition = (0, playerPosition.Column + 2);
+                        else if (playerPosition.Row >= 1 && playerPosition.Column >= rooms[1].Length - 2)
+                            playerPosition = (--playerPosition.Row, rooms[i].Length - 1);
+                        else
+                            playerPosition = (--playerPosition.Row, playerPosition.Column + 2);
+
+                        DisplayDescriptiveText("You have found a maelstorm and it sends you away one room north and two rooms east.");
+                        break;
+                    }
+                }
             }
         }  
 
